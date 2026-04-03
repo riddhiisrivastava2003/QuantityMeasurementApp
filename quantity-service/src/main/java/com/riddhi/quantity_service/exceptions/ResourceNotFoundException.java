9e27ef0f53bd61a1fe9373abda2d4c9cb1ada7e3
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2026-04-03 11:32:04 +0530

    removed old folders

--- quantity-service/src/main/java/com/riddhi/quantity_service/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.riddhi.quantity_service.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
