41d0bfd079f4687345b244ee7e7a0a4bf6dbd171
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2026-04-09 10:53:06 +0530

    refactor

--- auth-service/src/main/java/com/riddhi/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/riddhi/auth_service/controller/AuthController.java
@@ -24,7 +24,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:3000"})
+@CrossOrigin(origins = {"*"})
 @Tag(name = "Authentication", description = "User registration, login, logout and profile management")
 public class AuthController {
 
